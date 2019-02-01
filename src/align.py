e2c3312c459c3331238a8c4ef94028b2ce6ac107
Faisal Khan <faisal@Faisals-MacBook-Pro.local>
2019-02-01 00:30:00 -0600

    Added some data files

--- src/align.py
+++ src/align.py
@@ -2,21 +2,25 @@ import numpy as np
 import cv2
 from skimage.io import imread, imsave
 from skimage.transform import resize
+import matplotlib.pyplot as plt
 
 
 class Alignment:
     def __init__(self, image):
         self.image = image
 
-        if type(a).__module__ == np.__name__:
+        if type(image).__module__ == np.__name__:
             self.image_ = np.copy(image)
         else:
             self.image_ = imread(image)
+
+        self.image_ = (self.image_ - np.min(self.image_) ) / np.max(self.image_)
 
         self.parameters = {
             'binary_threshold': 0.25,
             'canny_thresh1': 0.5,
             'canny_thresh2': 1.0,
+            'transpose': False,
             'debug': True
         }
 
@@ -24,16 +28,33 @@ class Alignment:
         for k,v in params.items():
             self.parameters[k] = v
 
+        if self.parameters['transpose']:
+            self.image_ = np.transpose(self.image_)
+
         binary_threshold = self.parameters['binary_threshold']
+
         canny1 = self.parameters['canny_thresh1']
         canny2 = self.parameters['canny_thresh2']
 
-        binary = np.zeros_like(img2)
-        binary[img2 > 0.4] = 1      
+
+        binary = np.zeros_like(self.image_)
+        binary[self.image_ > binary_threshold] = 1      
         binary = np.uint8(binary)
 
-        if self.parmaeters['debug']:
-            imsave('binary_threshold.jpg', binary)
+        edges = cv2.Canny(binary, canny1, canny2, apertureSize = 7)
+
+     
+        x, y = np.nonzero(edges > 0)
+        print(np.max(x), np.min(x))
+        print(np.max(y), np.min(y))
+
+        plt.imshow(edges, cmap='gray')
+        plt.show()
+        
+        if self.parameters['debug']:
+            imsave('original.png', self.image_)
+            imsave('binary_threshold.png', binary * 255)
+            imsave('edges.png', np.uint8(edges))
 
 
 
--- src/main.py
+++ src/main.py
@@ -3,12 +3,20 @@ import click
 from align import Alignment
 
 @click.command()
-@click.option('-i', '--image_file')
+@click.option('-i', '--image_file', required=True)
+@click.option('-b', '--binary_threshold', default=0.4)
+@click.option('-c1', '--canny_low', default=0.5)
+@click.option('-c2', '--canny_high', default=0.5)
+@click.option('--transpose/--no-transpose', default=False)
 @click.option('--debug/--no-debug', default=True)
-def driver(image_file, debug):
+def driver(image_file, binary_threshold, canny_low, canny_high, transpose, debug):
     align = Alignment(image_file)
     params = {
-        'debug' = debug
+        'binary_threshold': binary_threshold,
+        'canny_thresh1': canny_low,
+        'canny_thresh2': canny_high,
+        'transpose': transpose,
+        'debug': debug
     }
     align.align(params)
 
